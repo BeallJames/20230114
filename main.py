40e85b35d71eb6482e702b0b299d00067510bbb9
bealljamesBC2022 <bealljamesgithub@gmail.com>
2023-01-14 16:55:29 -0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number: 
+        guess = int(input(f'Guess a Number between 1 and {x}: '))
+        if guess < random_number:
+            print(f'higher ')
+        elif guess > random_number:
+            print(f'lower ')
+        elif guess == random_number:
+            print(f'correct, {random_number}! ')
+guess(10)
